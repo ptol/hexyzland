715e11a124f097f3259566b039e6558bb0148f08
qwe <qwe>
2022-06-26 20:01:52 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'js/[name].[contenthash].js',
-    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
